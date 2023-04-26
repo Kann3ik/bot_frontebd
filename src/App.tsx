9c6d7ff8e5430913730b26f82031b82aa98dfbe0
Sergey <vov4ok2000@yandex.ru>
2023-04-26 14:16:18 +0300

    initial web application to telegram bot

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// @ts-ignore
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+  const onClose = () => {
+    tg.close();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={onClose}>Close</button>
       </header>
     </div>
   );
